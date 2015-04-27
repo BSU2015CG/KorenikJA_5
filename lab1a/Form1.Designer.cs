c331adfe2da9a3d88f1a251af0f3662292c6604c
yuliya_karenik <juliekorenik@gmail.com>
2015-04-27 23:20:00 +0300

    lab1a

---
+++ lab1a/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace lab2CG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPalette = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.pictureBoxG = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.pictureBoxR = new System.Windows.Forms.PictureBox();
+            this.labelR = new System.Windows.Forms.Label();
+            this.labelG = new System.Windows.Forms.Label();
+            this.labelB = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelPalette
+            // 
+            this.labelPalette.BackColor = System.Drawing.SystemColors.Control;
+            this.labelPalette.Location = new System.Drawing.Point(12, 9);
+            this.labelPalette.Name = "labelPalette";
+            this.labelPalette.Size = new System.Drawing.Size(328, 319);
+            this.labelPalette.TabIndex = 58;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(12, 348);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 62;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // pictureBoxG
+            // 
+            this.pictureBoxG.Location = new System.Drawing.Point(383, 131);
+            this.pictureBoxG.Name = "pictureBoxG";
+            this.pictureBoxG.Size = new System.Drawing.Size(256, 123);
+            this.pictureBoxG.TabIndex = 65;
+            this.pictureBoxG.TabStop = false;
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(383, 260);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(256, 123);
+            this.pictureBoxB.TabIndex = 64;
+            this.pictureBoxB.TabStop = false;
+            // 
+            // pictureBoxR
+            // 
+            this.pictureBoxR.Location = new System.Drawing.Point(383, 2);
+            this.pictureBoxR.Name = "pictureBoxR";
+            this.pictureBoxR.Size = new System.Drawing.Size(256, 123);
+            this.pictureBoxR.TabIndex = 63;
+            this.pictureBoxR.TabStop = false;
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = true;
+            this.labelR.Location = new System.Drawing.Point(664, 29);
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(26, 13);
+            this.labelR.TabIndex = 66;
+            this.labelR.Text = "Red";
+            // 
+            // labelG
+            // 
+            this.labelG.AutoSize = true;
+            this.labelG.Location = new System.Drawing.Point(664, 157);
+            this.labelG.Name = "labelG";
+            this.labelG.Size = new System.Drawing.Size(36, 13);
+            this.labelG.TabIndex = 67;
+            this.labelG.Text = "Green";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(664, 288);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(27, 13);
+            this.labelB.TabIndex = 68;
+            this.labelB.Text = "Blue";
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 424);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.labelG);
+            this.Controls.Add(this.labelR);
+            this.Controls.Add(this.pictureBoxG);
+            this.Controls.Add(this.pictureBoxB);
+            this.Controls.Add(this.pictureBoxR);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.labelPalette);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPalette;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.PictureBox pictureBoxG;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.PictureBox pictureBoxR;
+        private System.Windows.Forms.Label labelR;
+        private System.Windows.Forms.Label labelG;
+        private System.Windows.Forms.Label labelB;
+    }
+}
+
---
+++ lab1a/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab2CG
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bitmap, tmp;
+
+
+        int[] r;
+        int[] g;
+        int[] b;
+        const int SIZE = 256;
+        int brScrollValue;
+        public Form1()
+        {
+
+            r = new int[SIZE];
+            g = new int[SIZE];
+            b = new int[SIZE];
+            brScrollValue = 0;
+            InitializeComponent();
+        }
+
+        private void getPixels()
+        {
+            Size size = tmp.Size;
+            Color rgb;
+            for (int i = 0; i < SIZE; i++)
+            {
+                r[i] = 0;
+                g[i] = 0;
+                b[i] = 0;
+            }
+
+            for (int i = 0; i < size.Height; i++)
+                for (int j = 0; j < size.Width; j++)
+                {
+                    rgb = tmp.GetPixel(j, i);
+                    r[rgb.R]++;
+                    g[rgb.G]++;
+                    b[rgb.B]++;
+                }
+            labelR.Text = "Red average: " + GetAverage(r);
+            labelG.Text = "Green average: " + GetAverage(g);
+            labelB.Text = "Blue average: " + GetAverage(b);
+            pictureBoxR.CreateGraphics().Clear(SystemColors.Control);
+            pictureBoxG.CreateGraphics().Clear(SystemColors.Control);
+            pictureBoxB.CreateGraphics().Clear(SystemColors.Control);
+            Invalidate();
+        }
+
+        private int max(int[] array)
+        {
+            int max = 0;
+            for (int i = 0; i < SIZE; i++)
+                if (array[i] > max)
+                    max = array[i];
+            return max;
+
+        }
+
+        private double GetAverage(int[] array)
+        {
+            double sum = 0, amount = 0;
+            for (int i = 0; i < SIZE; i++)
+            {
+                sum += array[i] * i;
+                amount += array[i];
+            }
+            return sum / amount;
+        }
+
+        private void DrawHist(Color rgb, int[] array, PictureBox pb)
+        {
+            int m = max(array);
+            Pen pen = new Pen(rgb);
+            if (m != 0)
+                for (int i = 0; i < pb.Width; ++i)
+                    pb.CreateGraphics().DrawLine(pen, new Point(i, pb.Height),
+                                    new Point(i, (int)(pb.Height - (double)pb.Height * array[(int)((double)i * SIZE / pb.Width)] / m)));
+           
+
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.InitialDirectory = "d:\\projects\\КГ\\lab2_testPic";
+            ofd.Filter = "All files (*.*)|*.*";
+            ofd.FilterIndex = 2;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                Stream stream = null;
+                try
+                {
+                    if ((stream = ofd.OpenFile()) != null)
+                    {
+                        bitmap = new Bitmap(stream);
+                        //bitmap = new Bitmap(new Bitmap(stream), labelPalette.Width, labelPalette.Height);
+                        labelPalette.Image = bitmap;
+                        tmp = new Bitmap(bitmap);
+                        getPixels();
+                        
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawHist(Color.Red, r, pictureBoxR);
+            DrawHist(Color.Green, g, pictureBoxG);
+            DrawHist(Color.Blue, b, pictureBoxB);
+        }
+
+        private int Truncate(int value){
+      if (value < 0)  value = 0;
+      if (value > 255) value = 255;
+      return value;
+        }
+    }
+}
